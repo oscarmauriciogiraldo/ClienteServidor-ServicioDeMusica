68feca1bbb8ee7ac066fdfc3e7e8144b98d82b3b
Oscar Giraldo <oscardan@utp.edu.co>
2021-03-15 20:26:28 -0500

    icono-reproductor-mensaje

--- REPRODUCTOR/player3.py
+++ REPRODUCTOR/player3.py
@@ -1,8 +1,16 @@
+
 from tkinter import *
 
 root = Tk()
 root.geometry('300x300')
 root.title("SputiFai")
 #root.iconbitmap(r'SputiFai.ico')
+
+text = Label(root, text='Lista de Reproduccion!')
+text.pack()
+
+photo = PhotoImage(file='ondas-sonoras.png')
+labelphoto = Label(root, image = photo)
+labelphoto.pack()
 
 root.mainloop()
