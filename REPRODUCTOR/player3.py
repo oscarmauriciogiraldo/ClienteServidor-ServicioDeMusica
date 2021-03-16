ce432d610cd92a9ddb4d8d5840615c64c1d4d8fc
Oscar Giraldo <oscardan@utp.edu.co>
2021-03-15 20:31:29 -0500

    boton

--- REPRODUCTOR/player3.py
+++ REPRODUCTOR/player3.py
@@ -1,16 +1,20 @@
-
 from tkinter import *
 
 root = Tk()
 root.geometry('300x300')
 root.title("SputiFai")
-#root.iconbitmap(r'SputiFai.ico')
+#root.iconbitmap(r'melody.ico')
 
 text = Label(root, text='Lista de Reproduccion!')
 text.pack()
 
+
+def play_btn():
+    print("Hey! This play button works pretty well!")
+
+
 photo = PhotoImage(file='ondas-sonoras.png')
-labelphoto = Label(root, image = photo)
-labelphoto.pack()
+btn = Button(root, image=photo, command=play_btn)
+btn.pack()
 
 root.mainloop()
