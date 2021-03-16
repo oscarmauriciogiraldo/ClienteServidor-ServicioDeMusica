2a928e76e0077c24ba59c8fe5f3a77e24867d857
Oscar Giraldo <oscardan@utp.edu.co>
2021-03-15 20:17:46 -0500

    icono-ventama

--- REPRODUCTOR/player3.py
+++ REPRODUCTOR/player3.py
@@ -1,8 +1,8 @@
 from tkinter import *
 
 root = Tk()
-#root.geometry('300x300')
+root.geometry('300x300')
 root.title("SputiFai")
-root.iconbitmap(r'sputifai.ico')
+#root.iconbitmap(r'SputiFai.ico')
 
 root.mainloop()
