40b188d4a9ea62b6c1c44cbb8b4703f824c8fd54
Oscar Giraldo <oscardan@utp.edu.co>
2021-03-23 17:49:06 -0500

    PLAY-MUSIC

--- CASO-DE-ESTUDIO/client-sputifay.py
+++ CASO-DE-ESTUDIO/client-sputifay.py
@@ -76,6 +76,30 @@ def main():
         elif cmd == "BORRAR":
             socket_client.send(f"{cmd}@{data[1]}".encode(FORMAT))
 
+        elif cmd == "PLAY":
+            while True:
+                print('Pulsa p para detenter la cancion')
+                print('Pulsa r para reanudar la cancion')
+                print('pulsa e para elegir otra cancion')
+                print('Pulsa s para salir')
+
+                opcion = input(">>> ")
+                """
+                if opcion=="p":
+                    mixer.music.pause()
+                elif opcion=="r":
+                    mixer.music.unpause()
+                elif opcion=="s":
+                    mixer.music.stop()
+                    break
+                elif opcion=="e":
+                    mixer.music.stop
+                    cancion=str(input('Seleccione la cancion: '))
+                    mixer.music.load(cancion)
+                    mixer.music.set_volume(0.7)
+                    mixer.music.play()
+                """
+
     
     print("Desconectado del servidor")
     socket_client.close()
